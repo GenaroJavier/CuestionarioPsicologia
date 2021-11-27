339ddcb71f5b5897ef413138ba541b42afd38aab
Miguel002004 <miguel002004@hotmail.com>
2021-11-27 12:57:39 -0600

    arreglando motor de plantillas

--- app.js
+++ app.js
@@ -5,19 +5,19 @@ var calcular = require('./calcular')
 var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static('public'));
+app.use("/public",express.static('public'));
 
 app.set("view engine", "pug");
 
 
 app.get("/",function(req,res) {
-    res.render("index1",{readcsv});
+    res.render("index_contenido",{readcsv});
   });
 
 app.post("/cuestionairo",function(req,res) {
     //console.log(req.body);
     var resul = calcular(req.body);
-    res.render("resultados",{resultado:resul})
+    res.render("cuestionario",{readcsv})
 });
 
 app.listen(8080);
