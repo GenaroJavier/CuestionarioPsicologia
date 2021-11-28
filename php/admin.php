15a88c1647542faaa4fb05c8e2124583c5bbd34b
Miguel002004 <miguel002004@hotmail.com>
2021-11-28 12:06:07 -0600

    ya califica, las respuestas se muestran en consola por ahora

--- php/admin.php
+++ php/admin.php
@@ -3,9 +3,9 @@ $row = 1;
 if (($handle = fopen("TestHablilidadesPensamiento1.csv", "r")) !== FALSE) {
   while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
     echo '<p>'.$data[0].'-'.$data[1].'</p>
-          <input type="radio" name="'.$data[0].'" value="v">
+          <input type="radio" name="'.$data[0].'" value="v" class="radiobtn">
           <label for="'.$data[0].'">Verdadero</label>
-          <input type="radio" name="'.$data[0].'" value="f">
+          <input type="radio" name="'.$data[0].'" value="f" class="radiobtn">
           <label for="'.$data[0].'">falso</label>';
   }
   fclose($handle);
